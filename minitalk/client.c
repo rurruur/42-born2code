6d42afe62ce09921b5c62002285965d6f5b8b8f0
Nakyeong Kim <kdkeiie8@gmail.com>
2022-03-29 17:31:47 +0900

    구현 완.

--- minitalk/client.c
+++ minitalk/client.c
@@ -3,10 +3,51 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nakkim <nakkim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: nakkim <nakkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 21:20:34 by nakkim            #+#    #+#             */
-/*   Updated: 2022/03/28 21:20:36 by nakkim           ###   ########.fr       */
+/*   Updated: 2022/03/29 17:27:48 by nakkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <signal.h>
+#include "./libft/libft.h"
+
+void	send_char(int pid, char ch)
+{
+	int	count;
+	int	result;
+
+	count = 7;
+	while (count-- > 0)
+	{
+		if (ch % 2 == 0)
+			result = kill(pid, SIGUSR1); // 30
+		else
+			result = kill(pid, SIGUSR2); // 31
+		if (result == -1)
+			exit(1);
+		ch /= 2;
+		usleep(100);
+	}
+}
+
+void	deliver(char *pid, char *str)
+{
+	int	index;
+
+	index = -1;
+	while (str[++index])
+		send_char(ft_atoi(pid), str[index]);
+	send_char(ft_atoi(pid), 127);
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+		return (1);
+	deliver(argv[1], argv[2]);
+
+	return (0);
+}
--- minitalk/server.c
+++ minitalk/server.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nakkim <nakkim@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: nakkim <nakkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 21:20:22 by nakkim            #+#    #+#             */
-/*   Updated: 2022/03/28 21:50:48 by nakkim           ###   ########.fr       */
+/*   Updated: 2022/03/29 17:30:34 by nakkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void printPID(int pid) {
 	char *str;
 
 	str = ft_itoa(pid);
+	if (!str)
+		exit(1);
 	write(1, "process id: ", 12);
 	write(1, str, ft_strlen(str));
 	write(1, "\n", 1);
@@ -25,22 +27,41 @@ void printPID(int pid) {
 }
 
 void signal_handler(int sig) {
-	write(1, "signal handler\n", 15);
+	static int	count;
+	static char	ch;
+
+	if (--count < 1)
+	{
+		count = 7;
+		ch = 0;
+	}
+	ch |= ((sig % 2)<<(7-count));
+	if (count == 1)
+	{
+		if (ch == 127)
+			write(1, "\n", 1);
+		else
+			write(1, &ch, 1);
+	}
 }
 
 int main(void)
 {
 	struct sigaction act;
+	int		result;
 
 	act.sa_handler = signal_handler;
-	sigemptyset(&act.sa_mask); // ?
+	// sa_mask: 시그널 처리하는 동안 블록시킬 시그널 집합인데 비워뒀으므로 모든 시그널 블록되지 않음
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = 0; // sa_flag?
+	result = sigaction(SIGUSR1, &act, NULL);
+	result += sigaction(SIGUSR2, &act, NULL);
+	if (result != 0)
+		exit(1);
 	printPID(getpid());
-	sigaction(SIGUSR1, &act, 0);
 
-	// kill -SIGUSR1 [pid] -> 핸들러 실행
 	while (1) {
-		write(1, "--\n", 3);
-		sleep(1);
+		pause();
 	}
 
 	return (0);
