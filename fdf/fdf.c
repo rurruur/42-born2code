39fe3ae73d4953b968b2322fe55ac57bb3bfdef8
Nakyeong Kim <kdkeiie8@gmail.com>
2022-04-14 16:18:36 +0900

    [fdf] 백업 - draw 작업중

--- fdf/fdf.c
+++ fdf/fdf.c
@@ -6,7 +6,7 @@
 /*   By: nakkim <nakkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 23:40:17 by nakkim            #+#    #+#             */
-/*   Updated: 2022/04/14 15:30:28 by nakkim           ###   ########.fr       */
+/*   Updated: 2022/04/14 16:16:53 by nakkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,74 @@
 
 int key_hook(int keycode, void* param)
 {
-	printf("%d\n", keycode);
+	(void)param;
 	if (keycode == ESC)
 		exit(1);
 	return (1);
 }
 
-int	main(int argc, char** argv)
+void draw_point(void *mlx_ptr, void *win_ptr, int x, int y, int z)
 {
-	void*	mlx_ptr;
-	void*	win_ptr;
-	void*	param;
+	int		i;
 
+	i = -5;
+	while (i < 5)
+	{
+		mlx_pixel_put(mlx_ptr, win_ptr, x + i, y, 0x00FFFFFF);
+		mlx_pixel_put(mlx_ptr, win_ptr, x, y + i, 0x00FFFFFF);
+		i++;
+	}
+}
+
+void draw_map(void *mlx_ptr, void *win_ptr, char *map_file)
+{
+	int		fd;
+	int		x;
+	int		y;
+	int		i;
+	char	*result;
+
+	fd = open(map_file, O_RDONLY);
+	x = 100;
+	y = 200;
+	if (fd == -1)
+		exit(1);
+	
+	result = get_next_line(fd);
+	while (result)
+	{
+		y += 10;
+		i = 0;
+		printf("%s", result);
+		while (result[i] && result[i] != '\n')
+		{
+			x += 10;
+			draw_point(mlx_ptr, win_ptr, x, y, result[i]);
+			i++;
+		}
+		free(result);
+		result = get_next_line(fd);
+	}
+	
+	close(fd);
+}
+
+int	main(int argc, char **argv)
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	void	*param;
+
+	if (argc != 2)
+		return (0);
+	param = NULL;
 	mlx_ptr = mlx_init();
 	win_ptr = mlx_new_window(mlx_ptr, SIZE_X, SIZE_Y, TITLE);
 	mlx_key_hook(win_ptr, key_hook, param);
+	mlx_clear_window(mlx_ptr, win_ptr);
+
+	draw_map(mlx_ptr, win_ptr, argv[1]);
+
 	mlx_loop(mlx_ptr);
 	// mlx_loop_hook(mlx_ptr, key_hook, param);
 	// mlx_clear_window(mlx_ptr, win_ptr);
--- fdf/fdf.h
+++ fdf/fdf.h
@@ -6,7 +6,7 @@
 /*   By: nakkim <nakkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 23:40:19 by nakkim            #+#    #+#             */
-/*   Updated: 2022/04/14 15:28:31 by nakkim           ###   ########.fr       */
+/*   Updated: 2022/04/14 15:53:58 by nakkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,14 @@
 # define FDF_H
 
 # include "./minilibx/mlx.h"
+# include "./get_next_line/get_next_line.h"
 # include <fcntl.h> // open
 # include <unistd.h> // read, write, close
 # include <stdlib.h> // malloc, free, exit
 # include <stdio.h> // perror, strerror
 
-# define SIZE_X 500
-# define SIZE_Y 300
+# define SIZE_X 1000
+# define SIZE_Y 800
 # define TITLE "hi"
 # define ESC 53
 
