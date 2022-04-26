2fc7385607dadfe87c1f9eaca4690c44de18c40d
nakkim <kdkeiie8@gmail.com>
2022-04-26 23:50:23 +0900

    [fdf] 헤더 파일, Makefile 빼먹었당

--- fdf/fdf.h
+++ fdf/fdf.h
@@ -6,7 +6,7 @@
 /*   By: nakkim <nakkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 23:40:19 by nakkim            #+#    #+#             */
-/*   Updated: 2022/04/20 15:22:34 by nakkim           ###   ########.fr       */
+/*   Updated: 2022/04/26 23:02:58 by nakkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,14 @@ typedef struct bresenham
 	int	dy;
 	int	distance;
 }	t_bre;
+
+typedef struct map
+{
+	int	**info;
+	int	col_size;
+	int	row_size;
+}	t_map;
+
+void	bresenham(int x0, int y0, int x1, int y1, void* mlx_ptr, void* win_ptr);
 
 #endif
